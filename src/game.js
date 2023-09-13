e518f2e5755ed9403ed818ba1565d3021067ca0c
Marty_276 <martybedoya10@gmail.com>
2023-09-13 14:46:22 -0400

    bug fixed & basic tower rules added

--- src/game.js
+++ src/game.js
@@ -6,12 +6,14 @@ import {
     change_box_class,
     detect_click_target
 } from "./tools";
+import { tower_move } from "./moves_calculator"
 
 import { useState, useEffect } from "react";
 
 export const Game = () => {
 
     let board = empty_board;
+    let possible_moves = new Array;
     var positions = default_positions;
 
     function draw_pieces(){
@@ -37,11 +39,15 @@ export const Game = () => {
             if (moving_piece === null && box_clicked["piece"] !== "EM"){
                 
                 moving_piece = box_clicked;
+                possible_moves = tower_move(moving_piece, board);
 
             }else if (moving_piece !== null){
 
-                clear_moved_piece(moving_piece);
-                move_piece(moving_piece, box_clicked);
+                if (possible_moves.includes(box_clicked.id)){
+                    clear_moved_piece(moving_piece);
+                    move_piece(moving_piece, box_clicked);
+                }
+
                 moving_piece = null;
 
             }
@@ -55,10 +61,9 @@ export const Game = () => {
         }
 
         positions[moving_piece.piece].push(box_clicked.id);
-        board[moving_piece.id] = moving_piece.piece;
+        board[box_clicked.id] = moving_piece.piece;
         let box_element = document.getElementById(box_clicked.id);
         box_element.className = change_box_class(box_element.className, moving_piece.piece);
-
     }
 
     function clear_moved_piece(moving_piece){
---
+++ src/moves_calculator.js
@@ -0,0 +1,24 @@
+export function tower_move(piece_data, board){
+    
+    let possible_moves = new Array;
+
+    const x = piece_data.id.charAt(0);
+    const y = piece_data.id.charAt(1);
+    
+    for (let i = 1, np = ""; i <= 8; i++){
+        
+        np = i.toString() + y;
+        if (np != piece_data.id){
+            possible_moves.push(np);
+        }
+    }
+    for (let j = 1, np = ""; j <= 8; j++){
+        np = x + j.toString();
+        if (np != piece_data.id){
+            possible_moves.push(np);
+        }
+    }
+
+    return possible_moves;
+
+}
